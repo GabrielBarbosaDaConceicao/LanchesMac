c48627fb26175d503e397a4fd2450be55096eb5d
GabrielBarbosaDaConceicao <gabriel.conceicao@zarpsystem.com.br>
2022-06-14 14:20:57 -0300

    Adicionado meios para mostar todos os lanches

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,27 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            //var lanches = _lancheRepository.Lanches;
+            //return View(lanches);
+            var lancheListViewModel = new LancheListViewModel();
+            lancheListViewModel.Lanches = _lancheRepository.Lanches;
+            lancheListViewModel.CategoriaAtual = "Categoria Atual";
+
+            return View(lancheListViewModel);
+        }
+    }
+}
