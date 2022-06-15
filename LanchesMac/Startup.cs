957d0b24010a05b71dbc9999020cc999d18e4387
GabrielBarbosaDaConceicao <gabriel.conceicao@zarpsystem.com.br>
2022-06-15 08:43:17 -0300

    Criação e implementação do carrinho de compra

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -22,7 +22,12 @@ public class Startup
         services.AddTransient<ILancheRepository, LancheRepository>();
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
 
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
         services.AddControllersWithViews();
+
+        services.AddMemoryCache();
+        services.AddSession();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,9 +44,11 @@ public class Startup
             app.UseHsts();
         }
         app.UseHttpsRedirection();
-        app.UseStaticFiles();
 
+        app.UseStaticFiles();
         app.UseRouting();
+
+        app.UseSession();
 
         app.UseAuthorization();
 
