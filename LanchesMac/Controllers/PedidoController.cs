95a9ea41539527a90b58e452f0c70a8e253cc787
GabrielBarbosaDaConceicao <gabriel.conceicao@zarpsystem.com.br>
2022-06-17 14:57:13 -0300

    Adicionado view para checkout de pedido/PedidoController e implementação para cadastro ao finalisar o pedido e correção de erro no nome do botao de finalizar pedido

---
+++ LanchesMac/Controllers/PedidoController.cs
@@ -0,0 +1,32 @@
+﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, 
+            CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            return View();
+        }
+
+    }
+}
