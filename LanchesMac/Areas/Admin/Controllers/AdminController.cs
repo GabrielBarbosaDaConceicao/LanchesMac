ef03b7e9d075b7189495fcd05a0ca6212659714f
GabrielBarbosaDaConceicao <gabriel.bc.dev@hotmail.com>
2022-07-17 20:21:00 -0300

    Removido a pasta DAta/Model, add View _start-_layout/_inports/ControllerAdimin

---
+++ LanchesMac/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
