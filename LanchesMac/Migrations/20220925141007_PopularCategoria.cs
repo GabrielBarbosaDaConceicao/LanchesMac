dcf63164a95d3d0f54879556d0567795345f351c
Gabriel Conceicao <gabriel.bc.dev@hotmail.com>
2022-09-25 11:48:22 -0300

    Adicionado dados nas tabelas Categorias/Lanches via migrations

---
+++ LanchesMac/Migrations/20220925141007_PopularCategoria.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    public partial class PopularCategoria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO CATEGORIAS(CATEGORIANOME, DESCRICAO)" +
+                "VALUES('Normal', 'Lanche feito com ingredientes normais.')");
+
+            migrationBuilder.Sql("INSERT INTO CATEGORIAS(CATEGORIANOME, DESCRICAO) " +
+                "VALUES('Natural', 'Lanche feito com ingredientes integrais e naturais.')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM CATEGORIAS");
+        }
+    }
+}
