87b6e6309b6cb08825ea42ba2a3981ea1e017f46
Gabriel Conceicao <gabriel.conceicao@zarpsystem.com.br>
2022-09-26 20:12:57 -0300

    Adicionado LancheController/View para listar todos os lanches

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches.ToList();
+
+            return View(lanches);
+        }
+    }
+}
