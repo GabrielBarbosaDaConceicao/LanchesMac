9c7ace830f511176b071ada5a977c104f6bdc7fb
Gabriel Conceicao <gabriel.conceicao@zarpsystem.com.br>
2022-09-28 18:48:10 -0300

    Add LancheListViewModel e Partial View LanchesResumo

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
@@ -13,9 +14,11 @@ namespace LanchesMac.Controllers
 
         public IActionResult List()
         {
-            var lanches = _lancheRepository.Lanches.ToList();
+            var lanchesListViewModel = new LancheListViewModel();
+            lanchesListViewModel.Lanches = _lancheRepository.Lanches.ToList();
+            lanchesListViewModel.CategoriaAtual = "Todos os Lanches";
 
-            return View(lanches);
+            return View(lanchesListViewModel);
         }
     }
 }
