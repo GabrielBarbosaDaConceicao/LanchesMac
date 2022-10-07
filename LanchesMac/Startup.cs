fb799950a97abce0b2c7547a71a0be7fe4bb08a4
Gabriel Conceicao <gabriel.conceicao@zarpsystem.com.br>
2022-10-07 11:22:06 -0300

    Add controller CarrinhoCompraItem/CarrinhoCompraViewModel/Imagens/ViewIndex CarrinhoCompra

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Context;
+using LanchesMac.Models;
 using LanchesMac.Repositories;
 using LanchesMac.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -24,6 +25,7 @@ namespace LanchesMac
 
             services.AddTransient<ICategoriaRepository, CategoriaRepository>();
             services.AddTransient<ILancheRepository, LancheRepository>();
+            services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
