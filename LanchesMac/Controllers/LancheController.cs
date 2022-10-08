bb6abc1935bcfe3947050d0e59c43a375cbeea48
Gabriel Conceicao <gabriel.conceicao@zarpsystem.com.br>
2022-10-08 08:49:26 -0300

    Refatorado codigo para listas os lanches

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
-﻿using LanchesMac.Repositories.Interfaces;
+﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
 using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,12 +13,38 @@ namespace LanchesMac.Controllers
             _lancheRepository = lancheRepository;
         }
 
-        public IActionResult List()
+        public IActionResult List(string categoria)
         {
-            var lanchesListViewModel = new LancheListViewModel();
-            lanchesListViewModel.Lanches = _lancheRepository.Lanches.ToList();
-            lanchesListViewModel.CategoriaAtual = "Todos os Lanches";
+            IEnumerable<Lanche> lanches;
+            string categoriaAtual = string.Empty;
 
+            if (string.IsNullOrEmpty(categoria))
+            {
+                lanches = _lancheRepository.Lanches.OrderBy(l => l.LancheId);
+                categoriaAtual = "Todos os lanches";
+            }
+            else
+            {
+                if (string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase))
+                {
+                    lanches = _lancheRepository.Lanches
+                              .Where(l => l.Categoria.CategoriaNome.Equals("Normal"))
+                              .OrderBy(l => l.Nome);
+                }
+                else
+                {
+                    lanches = _lancheRepository.Lanches
+                              .Where(l => l.Categoria.CategoriaNome.Equals("Natural"))
+                              .OrderBy(l => l.Nome);
+                }
+                categoriaAtual = categoria;
+            }
+
+            var lanchesListViewModel = new LancheListViewModel()
+            {
+                Lanches = lanches,
+                CategoriaAtual = categoriaAtual
+            };
             return View(lanchesListViewModel);
         }
     }
